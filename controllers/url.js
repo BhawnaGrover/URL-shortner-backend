46ff950262b1c9dc6a67b70993cf138bdbbc087f
BhawnaGrover <bhawna0369.be21@chitkara.edu.in>
2024-05-08 21:17:17 +0530

    first commit

---
+++ controllers/url.js
@@ -0,0 +1,22 @@
+// require("nanoid");
+const URL = require('../models/url');
+
+async function handleGenerateNewShortURL(req, res){
+    const body = req.body;
+    if(!body.url) return res.status(400).json({error: 'url is required'})
+
+    const { nanoid } = await import('nanoid'); // dynamic import since nanoid doesnt work in commonjs
+
+    const shortID = nanoid(8);
+    await URL.create({
+        shortId : shortID,
+        redirectURL: body.url,
+        visitHistory: [],
+    });
+
+    return res.json({ id: shortID });
+}
+
+module.exports = {
+    handleGenerateNewShortURL,
+};
